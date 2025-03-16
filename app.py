a4daf3747e9d95c8fdbc324d6d9ffdf671835928
Aniket Tiwari <tiwarianiket921@gmail.com>
2025-03-16 23:34:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,6 @@ app = Flask(__name__)
 # Load the saved model and scaler
 model = pickle.load(open("FinalRFModel.pkl", 'rb'))
 scaler = pickle.load(open("scaler.pkl", 'rb'))
-encoders = pickle.load(open("encoder.pkl", 'rb'))
 
 @app.route('/')
 def home():
@@ -19,23 +18,13 @@ def predict():
         # Get form data
         form_data = request.form
 
-        # Encode categorical data using the LabelEncoders
-        encoded_category = encoders['category'].transform([form_data['category']])[0]
-        encoded_gender = encoders['gender'].transform([form_data['gender']])[0]
-        encoded_state = encoders['state'].transform([form_data['state']])[0]
-        encoded_day_name = encoders['day_name'].transform([form_data['day_name']])[0]
+        
 
         # Prepare features array
         features = np.array([[
-            encoded_category,
             float(form_data['amt']),
-            encoded_gender,
-            encoded_state,
-            int(form_data['zip']),  # Assuming ZIP is numeric
+            int(form_data['zip']),  
             float(form_data['City_pop']),
-            encoded_day_name,
-            float(form_data['Hour']),
-            float(form_data['distance_from_home']),
             float(form_data['age'])
         ]])
 
